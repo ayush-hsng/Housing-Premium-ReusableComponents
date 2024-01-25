05a9c20c550efc35d1eb8769bea31c79b48ec419
ayush-hsng <ayush.sinha@housing.com>
2024-01-25 12:13:04 +0530

    Creating SwiftUI Project

---
+++ HousingPremiumReusableComponents/HousingPremiumReusableComponents/HousingPremiumReusableComponentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HousingPremiumReusableComponentsApp.swift
+//  HousingPremiumReusableComponents
+//
+//  Created by Ayush Kumar Sinha on 25/01/24.
+//
+
+import SwiftUI
+
+@main
+struct HousingPremiumReusableComponentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
